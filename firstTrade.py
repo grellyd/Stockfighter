1abc6c2cbc320ab62854650b7d3b2457cddd9de7
Lellow <graham.brown137@gmail.com>
2016-05-05 02:57:46 -0700

    Specified qty, isBuy, and price for each ask or bid

--- firstTrade.py
+++ firstTrade.py
@@ -9,25 +9,44 @@ def getFullTicker():
         exit()
     r = requests.get(api + '/venues')
     venues_json = r.json()
-    print(r.text)
     for venue_dict in venues_json['venues']:
         venue = venue_dict['venue']
         r = requests.get(api + '/venues/' + venue )
         venue_json = r.json()
-        print('===============')
-        print('    ' + venue)
-        print('===============')
+        print('==========================')
+        print('       ' + venue)
+        print('==========================')
         for symbol_dict in venue_json['symbols']:
             symbol = symbol_dict['symbol']
             r = requests.get(api + '/venues/' + venue + '/stocks/' + symbol)
             symbol_json = r.json()
-            print('==========')
-            print('  ' + symbol)
-            print('==========')
-            print('== ASKS ==')
-            print (symbol_json['asks'])
-            print('== BIDS ==')
-            print (symbol_json['bids'])
+            print('====================')
+            print('       ' + symbol)
+            print('====================')
+            print('====== ASKS ======')
+            asks = symbol_json['asks']
+            if asks is not None:
+                for ask_dict in asks:
+                    qty = ask_dict['qty']
+                    isBuy = ask_dict['isBuy']
+                    price = ask_dict['price']
+                    print("qty: " , qty)
+                    print("isBuy: " , isBuy)
+                    print("price: " , price)
+            else:
+                print("None")
+            print('====== BIDS ======')
+            bids = symbol_json['bids']
+            if bids is not None:
+                for bid_dict in bids:
+                    qty = bid_dict['qty']
+                    isBuy = bid_dict['isBuy']
+                    price = bid_dict['price']
+                    print("qty: " , qty)
+                    print("isBuy: " , isBuy)
+                    print("price: " , price)
+            else:
+                print("None")
             print('')
         print('')
         print('')
@@ -35,7 +54,6 @@ def getFullTicker():
 def main():
     getFullTicker()
     r = requests.get(api + '/venues/TESTEX/stocks/FOOBAR')
-    print(r.text)
 
 if __name__ == "__main__":
     main()
